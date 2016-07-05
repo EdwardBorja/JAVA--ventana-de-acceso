dda6d520d3c29a1c9a42d33e0bff4fed870f7370
Eduardo <skate2612@hotmail.com>
2016-07-05 18:56:12 -0500

    1

---
+++ src/Ventanas/Conector.java
@@ -0,0 +1,26 @@
+package Ventanas;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+public class Conector {
+        static Connection conect = null;
+       public static Connection conexion()
+       {
+           try{
+               Class.forName("com.mysql.jdbc.Driver").newInstance();
+               
+               conect = DriverManager.getConnection("jdbc:mysql://localhost:3306/ejemplos","root","123");
+             
+               
+           }catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException e) {
+               JOptionPane.showMessageDialog(null, "Error" + e);
+        
+          }
+       return conect;
+       }
+}
